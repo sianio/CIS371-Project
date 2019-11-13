4e9382d78fd473158b16e347a5757db529336517
David Baas <Dcbaas@users.noreply.github.com>
2019-11-13 15:52:25 -0500

    started toolbars with material ui. deleted old toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import TestBar from './components/toolbar/toolbar-compositions/TestBar';
 import './App.css';
 
 
 const App = () => (
   <div className="App">
-    <TestBar />
     <h1>Hello React</h1>
   </div>
 );
