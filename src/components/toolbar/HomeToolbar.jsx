8c712194e37f6d893f9e6cbc419fa76dcfb2f400
David Baas <Dcbaas@users.noreply.github.com>
2019-11-25 16:08:59 -0500

    removed old imports for home bar

--- src/components/toolbar/HomeToolbar.jsx
+++ src/components/toolbar/HomeToolbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const homeStyle = makeStyles((theme) => ({
